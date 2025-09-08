420937b946353745b866634ea6b42a2536b9f7d8
klarhub <klarhubb@gmail.com>
2025-09-07 21:22:37 -0400

    Update OpenAI.js

--- api/OpenAI.js
+++ api/OpenAI.js
@@ -1,1 +1,24 @@
+import openai
+client = openai.OpenAI(api_key="key")
 
+response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "Answer this question correctly."},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "",
+                    },
+                },
+            ],
+        }
+    ],
+    max_tokens=600,
+)
+
+#print(response.choices[0])
+print(response.choices[0].message.content)
